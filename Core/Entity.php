0863ad77c76f46e67ed8bcf7be257a6ec91c868e
kevinalbrant <kevinalbrant@outlook.com>
2025-05-19 23:37:33 +0200

    Update Entity.php
    Added Variabled (UpdatedAt, CreatedAt) with the setters and getters

--- Core/Entity.php
+++ Core/Entity.php
@@ -11,6 +11,8 @@ abstract class Entity{
 
     // Common properties for all entities
     protected int $id;
+    protected \DateTime createdAt;
+    protected \DatemTime updatedAt;
 
     public function __construct(int $id){  
         $this->id = $id;
@@ -22,5 +24,21 @@ abstract class Entity{
 
     public function setId(int $id): void{
         $this->id = $id;
+    }
+
+    public function getCreatedAt(): \DateTime{
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime createdAt): void{
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): \DateTime{
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTime updatedAt): void{
+        $this->updatedAt = $updatedAt;
     }
 }
